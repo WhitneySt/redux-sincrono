6746a5b61908f71fb9d1806ed3e39226577b525b
WhitneySt <whitneystena418@gmail.com>
2022-11-03 09:44:23 -0500

    Agregando funcionalidad de suma y resta a la vista

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { restar, sumar } from "../redux/actions/counterActions";
 
 const App = () => {
+    const { counter } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+    
+    const minusOperation = () => {
+        dispatch(restar());
+    }
+    const addOperation = () => {
+      dispatch(sumar());
+    };
   return (
-      <div>
-          <h1>Contador</h1>
-          <section>
-              <button>-</button>
-              <span>1</span>
-              <button>+</button>
-          </section>
+    <div>
+      <h1>Contador</h1>
+      <section>
+        <button onClick={minusOperation}>-</button>
+        <span>{counter}</span>
+        <button onClick={addOperation}>+</button>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
